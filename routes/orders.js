e019747bd73a98ea4be9bb07c596ab1197d856c0
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-05-28 15:47:13 -0700

    add items on order endpoint

--- routes/orders.js
+++ routes/orders.js
@@ -12,20 +12,20 @@ const router = express.Router();
  * @apiName GetOrders
  * @apiGroup Orders
  *
- * @apiParam {Number} orderNumber (Optional) The order to look up.
+ * @apiParam {Number} ordernumber (Optional) The order to look up.
  *
  * @apiSuccess {Boolean} success      Request success.
  * @apiSuccess {Object[]} orders      List of orders.
  * @apiSuccess {Number} OrderNumber   Order number.
  * @apiSuccess {Number} StoreNumber   Store order was placed at.
- * @apiSuccess {Number} CustomerID    Customer order belongs to.
+ * @apiSuccess {Number} CustomerID    Customer that the order belongs to.
  * @apiSuccess {String} PickupMethod  Pickup method of order.
  * @apiSuccess {String} PaymentMethod Payment method of order.
  * @apiSuccess {String} OrderTime     Time order was placed.
  */
 router.get("/", (request, response, next) => {
-    if (request.query.ordernumber) {
-        request.query.ordernumber = parseInt(request.query.ordernumber);
+    if (request.query.orderNumber) {
+        request.query.orderNumber = parseInt(request.query.orderNumber);
         next();
     } else {
         const query = 'SELECT * FROM Orders ORDER BY OrderNumber';
@@ -39,14 +39,64 @@ router.get("/", (request, response, next) => {
         });
     }
 }, (request, response) => {
-    const query = `SELECT * FROM Orders WHERE OrderNumber = ?`;
-    const values = [request.query.ordernumber];
+    const query = 'SELECT * FROM Orders WHERE OrderNumber = ?';
+    const values = [request.query.orderNumber];
 
     pool.query(query, values, (error, results) => {
         if (error) throw error;
         if (results.length === 0) {
             response.send({
                 message: "No orders found."
+            });
+        } else {
+            response.send({
+                success: true,
+                orders: results
+            });
+        }
+    });
+});
+
+/**
+ * @api {get} /orders/items Request all items on order.
+ * @apiName GetOrderItems
+ * @apiGroup Orders
+ *
+ * @apiParam {Number} orderNumber (Optional) The order to look up.
+ *
+ * @apiSuccess {Boolean} success      Request success.
+ * @apiSuccess {Object[]} orders      List of orders.
+ * @apiSuccess {Number} OrderNumber   Order number.
+ * @apiSuccess {Number} StoreNumber   Store order was placed at.
+ * @apiSuccess {Number} CustomerID    Customer that the order belongs to.
+ * @apiSuccess {String} PickupMethod  Pickup method of order.
+ * @apiSuccess {String} PaymentMethod Payment method of order.
+ * @apiSuccess {String} OrderTime     Time order was placed.
+ */
+router.get("/items", (request, response, next) => {
+    if (request.query.orderNumber) {
+        request.query.orderNumber = parseInt(request.query.orderNumber);
+        next();
+    } else {
+        const query = 'SELECT * FROM OrderItems ORDER BY OrderNumber';
+
+        pool.query(query, (error, results) => {
+            if (error) throw error;
+            response.send({
+                success: true,
+                orders: results
+            });
+        });
+    }
+}, (request, response) => {
+    const query = 'SELECT * FROM OrderItems WHERE OrderNumber = ?';
+    const values = [request.query.orderNumber];
+
+    pool.query(query, values, (error, results) => {
+        if (error) throw error;
+        if (results.length === 0) {
+            response.send({
+                message: "No items found."
             });
         } else {
             response.send({
@@ -68,7 +118,7 @@ router.get("/", (request, response, next) => {
  * @apiSuccess {Object[]} orders      List of orders.
  * @apiSuccess {Number} OrderNumber   Order number.
  * @apiSuccess {Number} StoreNumber   Store order was placed at.
- * @apiSuccess {Number} CustomerID    Customer order belongs to.
+ * @apiSuccess {Number} CustomerID    Customer that the order belongs to.
  * @apiSuccess {String} PickupMethod  Pickup method of order.
  * @apiSuccess {String} PaymentMethod Payment method of order.
  * @apiSuccess {String} OrderTime     Time order was placed.
@@ -83,7 +133,7 @@ router.get("/customer", (request, response, next) => {
         });
     }
 }, (request, response) => {
-    const query = `SELECT * FROM Orders WHERE CustomerID = ? ORDER BY OrderNumber`;
+    const query = 'SELECT * FROM Orders WHERE CustomerID = ? ORDER BY OrderNumber';
     const values = [request.query.id];
 
     pool.query(query, values, (error, results) => {
