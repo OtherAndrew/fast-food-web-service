d9aca1ab8adaf3a98ff99d6072f173e847ceba9f
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-05-28 15:40:07 -0700

    add customer order history endpoint

--- routes/orders.js
+++ routes/orders.js
@@ -43,7 +43,50 @@ router.get("/", (request, response, next) => {
     const values = [request.query.ordernumber];
 
     pool.query(query, values, (error, results) => {
-        console.log(results)
+        if (error) throw error;
+        if (results.length === 0) {
+            response.send({
+                message: "No orders found."
+            });
+        } else {
+            response.send({
+                success: true,
+                orders: results
+            });
+        }
+    });
+});
+
+/**
+ * @api {get} /orders/customer Request order history of a customer.
+ * @apiName GetCustomerOrders
+ * @apiGroup Orders
+ *
+ * @apiParam {Number} id The customer ID to look up.
+ *
+ * @apiSuccess {Boolean} success      Request success.
+ * @apiSuccess {Object[]} orders      List of orders.
+ * @apiSuccess {Number} OrderNumber   Order number.
+ * @apiSuccess {Number} StoreNumber   Store order was placed at.
+ * @apiSuccess {Number} CustomerID    Customer order belongs to.
+ * @apiSuccess {String} PickupMethod  Pickup method of order.
+ * @apiSuccess {String} PaymentMethod Payment method of order.
+ * @apiSuccess {String} OrderTime     Time order was placed.
+ */
+router.get("/customer", (request, response, next) => {
+    if (request.query.id) {
+        request.query.id = parseInt(request.query.id);
+        next();
+    } else {
+        response.status(400).send({
+            message: "Missing required information",
+        });
+    }
+}, (request, response) => {
+    const query = `SELECT * FROM Orders WHERE CustomerID = ? ORDER BY OrderNumber`;
+    const values = [request.query.id];
+
+    pool.query(query, values, (error, results) => {
         if (error) throw error;
         if (results.length === 0) {
             response.send({
