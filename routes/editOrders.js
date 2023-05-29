1bb59978128fa34022519f58b79e919cda917b9a
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-05-28 20:57:33 -0700

    add item remove endpoint

--- routes/editOrders.js
+++ routes/editOrders.js
@@ -7,6 +7,75 @@ const pool = require('../utilities/exports').pool;
 const router = express.Router();
 
 /**
+ * @api {delete} /orders/item Request to remove an item from an order.
+ * @apiName DeleteOrder
+ * @apiGroup Orders
+ *
+ * @apiParam {Number} orderNumber The order to remove an item from.
+ * @apiParam {Number} itemNumber  The item to remove.
+ *
+ * @apiSuccess {Boolean} success Request success.
+ *
+ * @apiError (400: Missing Parameters) {String} message "Missing required information."
+ * @apiError (404: Order Not Found) {String}    message "No orders found."
+ * @apiError (404: Item Not Found) {String}     message "No items found."
+ */
+router.delete('/item', (request, response, next) => {
+    if (!request.query.orderNumber || !request.query.itemNumber) {
+        response.status(400).send({
+            message: "Missing required information."
+        });
+    } else {
+        next();
+    }
+}, (request, response, next) => {
+    const query = 'SELECT * FROM OrderItems WHERE OrderNumber = ?';
+    const values = [parseInt(request.query.orderNumber)];
+
+    pool.query(query, values, (error, results) => {
+        if (error) throw error;
+
+        if (results.length === 0) {
+            response.status(404).send({
+                message: "No orders found."
+            });
+        } else {
+            next();
+        }
+    });
+}, (request, response, next) => {
+    const query = 'SELECT * FROM OrderItems WHERE OrderNumber = ? AND ItemNumber = ?';
+    const values = [
+        parseInt(request.query.orderNumber),
+        parseInt(request.query.itemNumber)
+    ];
+
+    pool.query(query, values, (error, results) => {
+        if (error) throw error;
+        if (results.length === 0) {
+            response.status(404).send({
+                message: "No items found."
+            });
+        } else {
+            next();
+        }
+    });
+}, (request, response) => {
+    const query = 'DELETE FROM OrderItems WHERE OrderNumber = ? AND ItemNumber = ?'
+    const values = [
+        parseInt(request.query.orderNumber),
+        parseInt(request.query.itemNumber)
+    ];
+
+    pool.query(query, values, (error) => {
+        if (error) throw error;
+        response.send({
+            success: true
+        })
+    });
+});
+
+/**
  * @api {delete} /orders/cancel Request to delete an order.
  * @apiName DeleteOrder
  * @apiGroup Orders
