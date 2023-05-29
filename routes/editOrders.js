b6e01427f5dcb3ac0f8d73c3e2760545a6015290
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-05-28 22:45:38 -0700

    add order creation endpoint

--- routes/editOrders.js
+++ routes/editOrders.js
@@ -8,12 +8,138 @@ const pool = require('../utilities/exports').pool;
 const router = express.Router();
 
 /**
+ * @api {post} /orders Request to create a new order.
+ * @apiName CreateOrder
+ * @apiGroup Orders
+ *
+ * @apiParam {Number} storeNumber   The store to submit the order to.
+ * @apiParam {Number} customerID    The customer the order belongs to.
+ * @apiParam {String} pickupMethod  The order pickup method.
+ * @apiParam {String} paymentMethod The order payment method.
+ *
+ * @apiSuccess {Boolean} success Request success.
+ *
+ * @apiError (400: Missing Parameters) {String}  message "Missing required information."
+ * @apiError (400: Malformed Parameter) {String} message "Malformed parameter(s)."
+ * @apiError (400: Malformed Parameter) {String} message "Invalid pickup method."
+ * @apiError (400: Malformed Parameter) {String} message "Invalid payment method."
+ * @apiError (404: Store Not Found) {String}     message "No stores found."
+ * @apiError (404: Customer Not Found) {String}  message "No customers found."
+ */
+router.post('/', (request, response, next) => {
+    if (!request.body.storeNumber
+            || !request.body.customerID
+            || !request.body.pickupMethod
+            || !request.body.paymentMethod) {
+        response.status(400).send({
+            message: "Missing required information."
+        });
+    } else if (isNaN(request.body.storeNumber)
+            || isNaN(request.body.customerID)
+            || !isStringProvided(request.body.pickupMethod)
+            || !isStringProvided(request.body.paymentMethod)) {
+        response.status(400).send({
+            message: "Malformed parameter(s)."
+        });
+    } else {
+        next();
+    }
+}, (request, response, next) => {
+    // validate store exists
+    const query = 'SELECT * FROM StoreBranch WHERE StoreNumber = ?';
+    const values = [parseInt(request.body.storeNumber)];
+
+    pool.query(query, values, (error, results) => {
+        if (error) throw error;
+
+        if (results.length === 0) {
+            response.status(404).send({
+                message: "No stores found."
+            });
+        } else {
+            next();
+        }
+    });
+}, (request, response, next) => {
+    // validate customer exists
+    const query = 'SELECT * FROM Customers WHERE CustomerID = ?';
+    const values = [parseInt(request.body.customerID)];
+
+    pool.query(query, values, (error, results) => {
+        if (error) throw error;
+
+        if (results.length === 0) {
+            response.status(404).send({
+                message: "No customers found."
+            });
+        } else {
+            next();
+        }
+    });
+}, (request, response, next) => {
+    // validate pickup method exists
+    const query = 'SELECT * FROM PickupMethod WHERE Method = ?';
+    const values = [request.body.pickupMethod];
+
+    pool.query(query, values, (error, results) => {
+        if (error) throw error;
+
+        if (results.length === 0) {
+            response.status(400).send({
+                message: "Invalid pickup method."
+            });
+        } else {
+            next();
+        }
+    });
+}, (request, response, next) => {
+    // validate payment method exists
+    const query = 'SELECT * FROM PaymentMethod WHERE Method = ?';
+    const values = [request.body.paymentMethod];
+
+    pool.query(query, values, (error, results) => {
+        if (error) throw error;
+
+        if (results.length === 0) {
+            response.status(400).send({
+                message: "Invalid payment method."
+            });
+        } else {
+            next();
+        }
+    });
+}, (request, response) => {
+    // add order and return order id
+    const query =
+        'INSERT INTO Orders(StoreNumber, CustomerID, PickupMethod, PaymentMethod)\n' +
+        'VALUES (?, ?, ?, ?)';
+    const values = [
+        parseInt(request.body.storeNumber),
+        parseInt(request.body.customerID),
+        request.body.pickupMethod,
+        request.body.paymentMethod
+    ];
+
+    pool.query(query, values, (error, result) => {
+        if (error) throw error;
+        console.log(result.insertId)
+        console.log("sdsdfsdf")
+        response.send({
+            success: true,
+            orderID: result.insertId
+        });
+    });
+});
+
+/**
  * @api {post} /orders/item Request to add an item to an order.
  * @apiName AddOrderItem
  * @apiGroup Orders
  *
- * @apiParam {Number} orderNumber The order to add an item to.
- * @apiParam {Number} itemNumber  The item to add.
+ * @apiParam {Number} orderNumber              The order to add an item to.
+ * @apiParam {Number} itemNumber               The item to add.
+ * @apiParam {Number} quantity (Optional)      The item quantity.
+ * @apiParam {String} modifications (Optional) The item modifications.
  *
  * @apiSuccess {Boolean} success Request success.
  *
